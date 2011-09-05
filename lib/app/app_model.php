326e67a8a6c3c3f02fe3b49163a98a2c29893d5e
leo mendoza <leomendoza@gmail.com>
2011-09-05 15:19:21 -0400

    Revert "Added trim behavior in beforeSave. Fixes #27"
    Changes will be local to each model instead.
    
    This reverts commit 801b69bef39f008ffc2d7be7885c0237b8582557.

--- lib/app/app_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class AppModel extends Model {
-		function beforeSave($options) {
-			foreach($this->data[$this->name] as $key=>$value) {
-				$this->data[$this->name][$key] = trim($value);
-			}
-			return true;
-		}
-}
